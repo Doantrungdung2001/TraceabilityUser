0f87ec83843e7fdcb67100b6d172c37ec13e5c6d
doandung <doantrungdung2001@gmail.com>
2023-11-01 02:17:53 +0700

    video

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './home.css'
+import video from '../../Assets/Video/video1.mp4'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop 
+      type="video/mp4"></video>
+
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">
+            Our Packages
+          </span>
+          <h1 className="homeTitle">
+            Search Code
+          </h1>
+        </div>
+      </div>
+      
+    </section>
   )
 }
 
