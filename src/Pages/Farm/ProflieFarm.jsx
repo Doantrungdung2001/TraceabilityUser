00486d4d8abb3cdfc6aedbf589d8ef1f8b6b55b9
doandung <doantrungdung2001@gmail.com>
2023-11-16 15:01:17 +0700

    add project in profile of Farm

--- src/Pages/Farm/ProflieFarm.jsx
+++ src/Pages/Farm/ProflieFarm.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import Project from '../../Components/Project/Project'
 import Avatar from '../../Components/Avatar/Avatar'
+import './farm.css'
 
 const ProflieFarm = () => {
   return (
-    <div>
-        <Navbar />
-        <Avatar />
-        <Project />
+    <div className='layout-proflie-farm'>
+        <div className='white-gradient'>
+            <Navbar />
+            <Avatar />
+            <Project />
+        </div>
     </div>
   )
 }
