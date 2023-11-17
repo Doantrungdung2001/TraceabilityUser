f4aa70f161cbadf20d9cc1e52598e122dc750494
doandung <doantrungdung2001@gmail.com>
2023-11-17 14:53:47 +0700

    add intruduce in about-us page

--- src/Pages/AboutUs/About.jsx
+++ src/Pages/AboutUs/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
+import Introduce from '../../Components/Introduce/Introduce'
 
 const About = () => {
   return (
     <div>
       <Navbar/>
+      <Introduce/>
     </div>
   )
 }
