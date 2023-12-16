54ee3ae27248165b184c80f252663dd1e26d3eaf
doandung <doantrungdung2001@gmail.com>
2023-12-16 23:11:59 +0700

    update list farm

--- src/Pages/Farm/Farm.jsx
+++ src/Pages/Farm/Farm.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Main from "../../Components/Main/Main";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
-
+import ListFarm from "../../Components/Farm/ListFarm";
 const Farm = () => {
   return (
     <div>
       <Navbar />
-      <Main />
+      <ListFarm />
       <Footer/>
     </div>
   );
