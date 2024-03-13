39c8da950bed95ea0c93cd27e043ac40df6d4f0a
doandung <doantrungdung2001@gmail.com>
2024-03-14 05:34:04 +0700

    update css proflie farm

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -39,17 +39,27 @@ const ProfileFarm = () => {
             <div className="px-6">
               <div className="flex flex-wrap justify-center">
                 <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                  <div className="relative">
+                  {/* <div className="relative">
                     <img
                       alt="..."
                       src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg"
                       className="shadow-xl rounded-full h-auto align-middle border-none absolute -top-16 -left-20 lg:-left-16 max-w-150"
                     />
+                  </div> */}
+                  <div
+                    tabIndex="0"
+                    className="focus:outline-none h-44 w-44 mb-4 lg:mb-0 mr-4"
+                  >
+                    <img
+                      src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_3_7.png"
+                      alt="man avatar"
+                      className="h-full w-full rounded-full overflow-hidden shadow"
+                    />
                   </div>
                 </div>
 
                 <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                  <div className="py-6 px-3 mt-32 sm:mt-0">
+                  <div className="py-2 px-3 mt-8 sm:mt-0">
                     <button
                       className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                       type="button"
@@ -58,8 +68,8 @@ const ProfileFarm = () => {
                     </button>
                   </div>
                 </div>
-                <div className="w-full lg:w-4/12 px-4 lg:order-1">
-                  <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                <div className="w-full lg:w-4/12 px-2 lg:order-1">
+                  <div className="flex justify-center py-4 lg:pt-4 pt-4">
                     <div className="mr-4 p-3 text-center">
                       <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
                         22
@@ -83,7 +93,7 @@ const ProfileFarm = () => {
                   </div>
                 </div>
               </div>
-              <div className="text-center mt-12">
+              <div className="text-center mt-3">
                 <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
                   Jenna Stones
                 </h3>
