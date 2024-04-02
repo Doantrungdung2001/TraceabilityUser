27af249bcc263b3513f741b33e45e344a7fc9ba7
doandung <doantrungdung2001@gmail.com>
2024-04-03 06:03:56 +0700

    get api search

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,20 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import "./search.css";
 import video from "../../Assets/Video/video1.mp4";
-
-
 import Aos from "aos";
 import "aos/dist/aos.css";
+import { useNavigate } from "react-router-dom";
 
-const Search = ({projectId}) => {
+const Search = ({ projectId }) => {
+  const navigate = useNavigate();
+  const [inputData, setInputData] = useState('');
+
+  const handleChange = (event) => {
+    setInputData(event.target.value);
+  };
   // Create a react hook to add aa scroll animation
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
 
-  const handleClick = () => {
-    // window.location.href = '/';
-  }
+  const handleClick = ({ data }) => {
+    // if (projectId && data === projectId) {
+    //   navigate(`/results/${projectId}`);
+    // } else {
+    //   navigate(`/404-notfound`);
+    // }
+  };
 
   return (
     <section className="home">
@@ -30,9 +39,6 @@ const Search = ({projectId}) => {
 
       <div className="homeContent container">
         <div className="textDiv">
-          {/* <span data-aos="fade-up" className="smallText">
-            Here We Go
-          </span> */}
           <h1 data-aos="fade-up" className="homeTitle">
             Truy xuất nguồn gốc
           </h1>
@@ -44,9 +50,11 @@ const Search = ({projectId}) => {
               className="input"
               type="text"
               placeholder="Nhập mã code....."
+              value={inputData}
+              onChange={handleChange}
             />
-            <button className="btn flex" onClick={handleClick()}>
-              Search
+            <button className="btn flex" onClick={handleClick(inputData)}>
+              Tra
             </button>
           </div>
         </div>
