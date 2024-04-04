ec5c9ed94b7e989ec3ade9f815f13ed6bf0a726e
doandung <doantrungdung2001@gmail.com>
2024-04-04 21:51:32 +0700

    add result search for transaction and index

--- src/Components/Traceanility/SearchId.jsx
+++ src/Components/Traceanility/SearchId.jsx
@@ -3,26 +3,113 @@ import React from "react";
 const SearchId = () => {
   return (
     <section className="">
-      <div className="flex items-center justify-center min-h-screen bg-green-100">
-        <div className="mx-5 bg-white rounded-2xl border shadow-xl p-5 md:p-10 max-w-lg w-full md:w-3/4 lg:w-2/3 xl:w-1/2 ">
-          <div className="flex flex-col items-center space-y-4">
-            <h1 className="font-bold text-2xl text-gray-700 w-full text-center">
-              Index
-            </h1>
-            <p className="text-sm text-gray-500 text-center w-full">
-              Hãy nhập mã index sau đó kết qua tra cứu sẽ hiển thị.
-            </p>
-            <input
-              type="text"
-              placeholder="index"
-              className="border-2 rounded-lg w-full h-12 px-4"
-            />
-            <button className="bg-green-400 text-white rounded-md font-semibold px-4 py-3 w-full">
-              Tra cứu
-            </button>
+      <section>
+        <div className="flex items-center justify-center min-h-screen bg-green-100">
+          <div className="mx-5 bg-white rounded-2xl border shadow-xl p-5 md:p-10 max-w-lg w-full md:w-3/4 lg:w-2/3 xl:w-1/2 ">
+            <div className="flex flex-col items-center space-y-4">
+              <h1 className="font-bold text-2xl text-gray-700 w-full text-center">
+                Index
+              </h1>
+              <p className="text-sm text-gray-500 text-center w-full">
+                Hãy nhập mã index sau đó kết qua tra cứu sẽ hiển thị.
+              </p>
+              <input
+                type="text"
+                placeholder="index"
+                className="border-2 rounded-lg w-full h-12 px-4"
+              />
+              <button className="bg-green-400 text-white rounded-md font-semibold px-4 py-3 w-full">
+                Tra cứu
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
+
+      <section className="bg-gray-100 p-4 md:p-10 mx-2 md:mx-5">
+        <div class="bg-white overflow-hidden shadow rounded-lg border">
+          <div class="px-4 py-5 sm:px-6">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+              Thông tin
+            </h3>
+            <p class="mt-1 max-w-2xl text-sm text-gray-500">
+              This is some information about the user.
+            </p>
+          </div>
+          <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+            <dl class="sm:divide-y sm:divide-gray-200">
+              <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-500">Full name</dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  John Doe
+                </dd>
+              </div>
+              <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-500">Email address</dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  johndoe@example.com
+                </dd>
+              </div>
+              <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-500">Phone number</dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  (123) 456-7890
+                </dd>
+              </div>
+              <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-500">Address</dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  123 Main St Anytown, USA 12345
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+        <div className="m-8 ">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block">
+              <span className="text-transparent bg-clip-text bg-gradient-to-tr to-cyan-500 from-green-600">
+                Danh sách video
+              </span>
+            </span>
+          </h1>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+          <div className="flex flex-col items-center justify-center">
+            <div className="rounded-lg shadow-lg bg-white max-w-sm">
+              <a href="#!">
+                <video
+                  width="120"
+                  height="140"
+                  controls
+                  className="w-full rounded-t-lg"
+                >
+                  <source
+                    src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+                    type="video/mp4"
+                  />
+                  <source src="movie.ogg" type="video/ogg" />
+                  Your browser does not support the video tag.
+                </video>
+              </a>
+              <div className="p-6">
+                <h5 className="text-gray-900 text-xl font-medium mb-2">Tên</h5>
+                <p className="text-gray-700 text-base mb-4">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <button
+                  type="button"
+                  className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Chi tiết
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </section>
   );
 };
