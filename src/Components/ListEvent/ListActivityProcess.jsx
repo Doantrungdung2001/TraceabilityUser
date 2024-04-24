a7484057bb132c11b3383dd9002d58e2cc4163fb
doandung <doantrungdung2001@gmail.com>
2024-04-24 11:31:10 +0700

    update date in process infor

--- src/Components/ListEvent/ListActivityProcess.jsx
+++ src/Components/ListEvent/ListActivityProcess.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import {
   renderTypeProcess,
   formatTransactionHashTable,
+  formatDate,
 } from "../../Utils/helpers";
 import {
   Tabs,
@@ -168,8 +169,10 @@ const ListActivityProcess = ({ listActivity }) => {
                     }}
                   >
                     <td className="lg:pl-5 lg:pr-3 pl-2 whitespace-no-wrap">
-                      <div className="text-gray-400">Today</div>
-                      <div>07:45</div>
+                      {/* <div className="text-gray-400">Today</div> */}
+                      <div className="text-gray-400">
+                        {formatDate(activity.time)}
+                      </div>
                     </td>
                     <td className="px-2 py-2 whitespace-no-wrap">
                       <div className="leading-5 text-black font-medium text-base lg:text-2xl">
