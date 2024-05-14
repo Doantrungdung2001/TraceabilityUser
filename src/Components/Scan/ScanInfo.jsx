d569851fb3ce20542572917c57d6f8994c65dacb
doandung <doantrungdung2001@gmail.com>
2024-05-14 21:32:25 +0700

    add UI scan

--- src/Components/Scan/ScanInfo.jsx
+++ src/Components/Scan/ScanInfo.jsx
@@ -3,114 +3,200 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import useScan from "./useScan";
 import QR from "../../Services/qrService";
-import { formatDateTime, formatTransactionHashTable } from "../../Utils/helpers";
+import {
+  formatDateTime,
+  formatTransactionHashTable,
+} from "../../Utils/helpers";
 
 const ScanInfo = () => {
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
   const [scanned, setScanned] = React.useState(false);
   const [qrInfo, setQRInfo] = React.useState(null);
-  const projectId = useParams().projectId
-  const privateId = useParams().privateId
-  const { projectInfo,
+  const projectId = useParams().projectId;
+  const privateId = useParams().privateId;
+  const {
+    projectInfo,
     isSuccessProjectInfo,
     isLoadingProjectInfo,
     allDistributerWithQR,
     isSuccessQR,
-    isLoadingQR } = useScan({ projectId });
+    isLoadingQR,
+  } = useScan({ projectId });
 
-
-  const handleScanQR = async ({projectId, privateId }) => {
-    console.log("projectId: ", projectId)
-    console.log("privateId: ", privateId)
+  const handleScanQR = async ({ projectId, privateId }) => {
+    console.log("projectId: ", projectId);
+    console.log("privateId: ", privateId);
     setScanned(true);
     try {
       setLoading(true);
-      const res = await QR.scanQR({projectId, privateId})
-      if (res.status === 200 && res.data.metadata.message === "QR is already scanned in blockchain" || res.data.metadata.message === "QR is already scanned") {
+      const res = await QR.scanQR({ projectId, privateId });
+      if (
+        (res.status === 200 &&
+          res.data.metadata.message ===
+            "QR is already scanned in blockchain") ||
+        res.data.metadata.message === "QR is already scanned"
+      ) {
         setQRInfo({
           timeScanned: res.data.metadata?.scannedQR?.timeScanned,
           txScan: res.data.metadata?.scannedQR?.txScan,
-          firstScan: false
+          firstScan: false,
         });
 
         console.log("qrInfo: ", {
           timeScanned: res.data.metadata?.scannedQR?.timeScanned,
           txScan: res.data.metadata?.scannedQR?.txScan,
-          firstScan: false
-        })
+          firstScan: false,
+        });
       }
 
-      if (res.status === 200 && !res.data.metadata.message ) {
+      if (res.status === 200 && !res.data.metadata.message) {
         setQRInfo({
           timeScanned: res.data.metadata?.qrItem?.timeScanned,
           txScan: res.data.metadata?.txScan,
-          firstScan: true
+          firstScan: true,
         });
       }
       setLoading(false);
-      console.log("res: ", res)
+      console.log("res: ", res);
     } catch (error) {
-      console.log("error: ", error)
+      console.log("error: ", error);
       setError(error);
     }
   };
 
-  
-
   return (
-    <section className="" style={{ paddingTop: "10vh" }}>
-      {
-        isSuccessProjectInfo && isSuccessQR && (
-            <div style={{ paddingTop: "10vh" }}>
-                <h1>{projectInfo.plant.plant_name}</h1>
-                <h2>{projectInfo.description}</h2>
-                <h3>{projectInfo.projectIndex}</h3>
-                <h4>{projectInfo.farm.name}</h4>
-                
+    <section className="" style={{ paddingTop: "16vh" }}>
+      {isSuccessProjectInfo && isSuccessQR && (
+        <div className="p-6 bg-grey lg:ml-20 lg:mr-20">
+          <div className="bg-white overflow-hidden shadow rounded-lg border">
+            <div className="lg:py-5 px-4 py-3">
+              <div className="flex justify-between items-center text-sm">
                 <div>
-                    {allDistributerWithQR.map((distributer) => (
-                    <div key={distributer.name}>
-                        <h1>{distributer.name}</h1>
-                    </div>
-                    ))}
+                  <h3 className="text-lg leading-6 font-medium text-green-700">
+                    Thông tin chung
+                  </h3>
                 </div>
-                <button onClick={() => handleScanQR({projectId, privateId})} disabled={scanned}>Scan QR</button>
+                <div className="ml-4 bg-blue-400 lg:p-2 p-1 rounded-lg flex items-center space-x-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-8 h-8 text-white" // Kích thước lớn hơn và màu xanh
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+                    />
+                  </svg>
+
+                  <h1 className="text-white font-bold italic">
+                    Đã được ghi trên blockchain
+                  </h1>
+                </div>
+              </div>
             </div>
-        )
-      }
-      {
-        isLoadingProjectInfo || isLoadingQR && (
-            <div style={{ paddingTop: "10vh" }}>
-                Loading...
+            <div className="border-t border-gray-200 lg:px-4 lg:py-5 sm:p-0 px-5 py-1">
+              <dl className="sm:divide-y sm:divide-gray-200">
+                <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-700">Tên cây</dt>
+                  <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
+                    {projectInfo.plant.plant_name}
+                  </dd>
+                </div>
+                <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-700">Mô tả</dt>
+                  <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
+                    {projectInfo.description}
+                  </dd>
+                </div>
+                <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-700">
+                    Số hiệu dự án
+                  </dt>
+                  <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
+                    {projectInfo.projectIndex}
+                  </dd>
+                </div>
+                <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-700">
+                    Nông trại sản xuất
+                  </dt>
+                  <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
+                    {projectInfo.farm.name}
+                  </dd>
+                </div>
+                <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="text-sm font-medium text-gray-700">
+                    Cửa hàng cung cấp
+                  </dt>
+                  <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
+                    {allDistributerWithQR.map((distributer) => (
+                      <div key={distributer.name}>
+                        <h1>{distributer.name}</h1>
+                        <br />
+                      </div>
+                    ))}
+                  </dd>
+                </div>
+                <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt className="">
+                    <div className="ml-6 mb-5 text-base font-bold text-white">
+                      <button
+                        className="bg-green-400 px-4 py-2 rounded-lg hover:bg-green-500"
+                        onClick={() => handleScanQR({ projectId, privateId })}
+                        disabled={scanned}
+                      >
+                        Scan QR
+                      </button>
+                    </div>
+                  </dt>
+                  <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
+                    {isLoadingProjectInfo ||
+                      (isLoadingQR && (
+                        <div>Loading...</div>
+                      ))}
+                    {loading && (
+                      <div>Scanning QR...</div>
+                    )}
+                    {qrInfo && qrInfo.firstScan && (
+                      <div>
+                        <h1>QR is scanned for the first time</h1>
+                        <h2>Time: {formatDateTime(qrInfo.timeScanned)}</h2>
+                        <h3>
+                          Transaction:{" "}
+                          {formatTransactionHashTable({
+                            str: qrInfo.txScan,
+                            a: 8,
+                            b: 5,
+                          })}
+                        </h3>
+                      </div>
+                    )}
+                    {qrInfo && !qrInfo.firstScan && (
+                      <div>
+                        <h1>QR is already scanned</h1>
+                        <h2>Time: {formatDateTime(qrInfo.timeScanned)}</h2>
+                        <h3>
+                          Transaction:{" "}
+                          {formatTransactionHashTable({
+                            str: qrInfo.txScan,
+                            a: 8,
+                            b: 5,
+                          })}
+                        </h3>
+                      </div>
+                    )}
+                  </dd>
+                </div>
+              </dl>
             </div>
-        )
-      }
-      {
-        loading && (
-            <div style={{ paddingTop: "10vh" }}>
-                Scanning QR...
-            </div>
-        )
-      }
-      {
-        qrInfo && qrInfo.firstScan && (
-            <div style={{ paddingTop: "10vh" }}>
-                <h1>QR is scanned for the first time</h1>
-                <h2>Time: {formatDateTime(qrInfo.timeScanned)}</h2>
-                <h3>Transaction: {formatTransactionHashTable({str: qrInfo.txScan, a: 8, b: 5})}</h3>
-            </div>
-        )
-      }
-      {
-        qrInfo && !qrInfo.firstScan && (
-            <div style={{ paddingTop: "10vh" }}>
-                <h1>QR is already scanned</h1>
-                <h2>Time: {formatDateTime(qrInfo.timeScanned)}</h2>
-                <h3>Transaction: {formatTransactionHashTable({str: qrInfo.txScan, a: 8, b: 5})}</h3>
-            </div>
-        )
-      }
+          </div>
+        </div>
+      )}
     </section>
   );
 };
