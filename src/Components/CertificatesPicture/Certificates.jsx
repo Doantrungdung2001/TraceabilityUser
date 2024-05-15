3df3d3bf5431c79026de75c5826c4370b61b3524
doandung <doantrungdung2001@gmail.com>
2024-05-15 09:46:14 +0700

    fix cification picture

--- src/Components/CertificatesPicture/Certificates.jsx
+++ src/Components/CertificatesPicture/Certificates.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Certificates = ({ dataPicture, isSuccessCertificateImages }) => {
   const [active, setActive] = useState();
+
   useEffect(() => {
     if (dataPicture && dataPicture.length > 0)
       setActive(dataPicture[0].imgelink);
@@ -9,32 +10,32 @@ const Certificates = ({ dataPicture, isSuccessCertificateImages }) => {
 
   return (
     <div className="grid gap-4">
-      
       {dataPicture.length ? (
         <>
-        <div>
-              <img
-                className="h-auto w-full max-w-full rounded-lg object-cover object-center md:h-[480px]"
-                src={active}
-                alt=""
-              />
-            </div>
-            {
-              dataPicture.map(({ imgelink }, index) => (
-                <div key={index} >
-                  <div className="grid grid-cols-5 gap-4">
-                    <div>
-                      <img
-                        onClick={() => setActive(imgelink)}
-                        src={imgelink}
-                        className="h-15 max-w-full cursor-pointer rounded-lg object-cover object-center"
-                        alt="gallery-image"
-                      />
-                    </div>
-                  </div>
-                </div>
-              ))
-            }
+          <div>
+            <img
+              className="h-auto w-full max-w-full rounded-lg object-cover object-center md:h-[480px]"
+              src={active}
+              alt=""
+            />
+          </div>
+          <div className="grid grid-cols-5 gap-4">
+            {dataPicture.map(({ imgelink }, index) => (
+              <div key={index}>
+                <img
+                  onClick={() => setActive(imgelink)}
+                  src={imgelink}
+                  className="h-15 max-w-full cursor-pointer rounded-lg object-cover object-center"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    objectFit: "cover",
+                  }}
+                  alt="gallery-image"
+                />
+              </div>
+            ))}
+          </div>
         </>
       ) : (
         <div className="lg:text-2xl text-gray-400 text-base mt-5">
