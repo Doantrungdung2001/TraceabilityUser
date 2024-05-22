c93547cfaa301bfe62d3505a77ec13120fe506dc
doandung <doantrungdung2001@gmail.com>
2024-05-22 16:06:17 +0700

    update Evaluation information

--- src/Components/Information/InformationOverview.jsx
+++ src/Components/Information/InformationOverview.jsx
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
-import CarouselPicture from "../Picture/CarouselPicture";
 import {
   Button,
-  Dialog,
-  DialogHeader,
-  DialogBody,
-  DialogFooter,
-  Spinner,
   Drawer,
   Typography,
   IconButton,
@@ -14,23 +8,9 @@ import {
 import { formatDate, formatTransactionHashTable } from "../../Utils/helpers";
 import { useNavigate } from "react-router";
 
-const InformationOverview = ({
-  dataImage,
-  allDistributerWithQR,
-  isSuccessImage,
-  isLoadingImage,
-  dataInfoOverview,
-  totalConnectionLossBySeconds,
-  processWithoutObjectDetectionCount,
-  deletedItemCount,
-  editItemCount,
-  totalCamera,
-  totalItemCount,
-}) => {
+const InformationOverview = ({ allDistributerWithQR, dataInfoOverview }) => {
   const navigate = useNavigate();
-  const [open, setOpen] = React.useState(false);
 
-  const handleOpen = () => setOpen(!open);
   const [openTop, setOpenTop] = useState(false);
   const openDrawerTop = () => setOpenTop(true);
   const closeDrawerTop = () => setOpenTop(false);
@@ -38,32 +18,6 @@ const InformationOverview = ({
   return (
     <section className="justify-center">
       <div className="min-h-screen flex flex-col sm:p-14 md:p-20 justify-center bg-white rounded-2xl">
-        <div className="flex justify-center items-center">
-          <h3 className="text-green-700 font-semibold text-lg mr-2">
-            Mức độ tin tưởng: Trung bình
-          </h3>
-          <a
-            onClick={handleOpen}
-            className="text-blue-400 cursor-pointer  italic hover:text-blue-600"
-          >
-            (Chi tiết...)
-          </a>
-        </div>
-        <div className="flex flex-col justify-center items-center rounded-md text-lg mb-2 bg-green-500 p-3 ml-5 mr-5 mt-3 lg:ml-80 lg:mr-80">
-          <h3 className="text-white font-bold">
-            Mã dự án trên blockchain : {dataInfoOverview.projectIndex}
-          </h3>
-          <a
-            className="text-white text-sm italic cursor-pointer hover:text-blue-800"
-            onClick={() =>
-              navigate(`/search/index/${dataInfoOverview.projectIndex}`)
-            }
-          >
-            Ấn vào để xem chi tiết
-          </a>
-        </div>
-
-        <div></div>
         <div className="mx-auto max-w-6xl">
           <section className="font-sans text-black">
             <div className="[ lg:flex lg:items-center ] [ fancy-corners fancy-corners--large fancy-corners--top-left fancy-corners--bottom-right ]">
@@ -77,7 +31,7 @@ const InformationOverview = ({
                         </h3>
                         {/* <Button onClick={openDrawerTop}>Thông tin hướng dẫn sử dụng</Button> */}
                       </div>
-                      <div className="ml-4 bg-blue-400 lg:p-2 p-1 rounded-lg flex items-center">
+                      <div className="ml-4 bg-blue-400 lg:p-2 p-1 rounded-lg flex items-center px-4 lg:px-6">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -93,9 +47,7 @@ const InformationOverview = ({
                           />
                         </svg>
 
-                        <h1 className="text-white font-bold italic">
-                          Trusted
-                        </h1>
+                        <h1 className="text-white font-bold italic">Trusted</h1>
                       </div>
                     </div>
                   </div>
@@ -148,12 +100,14 @@ const InformationOverview = ({
                       {dataInfoOverview.status === "inProgress" && (
                         <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                           <dt className="text-sm font-medium text-gray-700">
-                            {dataInfoOverview?.endDate ? "Ngày kết thúc" : "Dự kiến ngày kết thúc"}
+                            {dataInfoOverview?.endDate
+                              ? "Ngày kết thúc"
+                              : "Dự kiến ngày kết thúc"}
                           </dt>
                           <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
-                            {
-                              dataInfoOverview?.endDate
-                                ? formatDate(dataInfoOverview?.endDate) : formatDate(dataInfoOverview?.expectedEndDate)}
+                            {dataInfoOverview?.endDate
+                              ? formatDate(dataInfoOverview?.endDate)
+                              : formatDate(dataInfoOverview?.expectedEndDate)}
                           </dd>
                         </div>
                       )}
@@ -234,7 +188,7 @@ const InformationOverview = ({
           </IconButton>
         </div>
         <Typography color="gray" className="mb-8 pr-4 font-normal">
-          Nội dung ghi ở đây 
+          Nội dung ghi ở đây
         </Typography>
         <div className="flex gap-2">
           <Button size="sm" variant="outlined">
@@ -243,65 +197,6 @@ const InformationOverview = ({
           <Button size="sm">Get Started</Button>
         </div>
       </Drawer>
-      <Dialog open={open} handler={handleOpen}>
-        <DialogHeader>Thông tin đánh giá</DialogHeader>
-        <DialogBody>
-          <div className="bg-grey">
-            <div className="bg-white overflow-hidden shadow rounded-lg border">
-              <div className="border-t border-gray-200 lg:px-4 lg:py-5 sm:p-0 px-5 py-1">
-                <dl className="sm:divide-y sm:divide-gray-200">
-                  <div className="lg:py-3 py-1 sm:py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 lg:grid-cols-2">
-                    <dt className="text-sm font-medium text-gray-700">
-                      Số thời gian camera bị mất kết nối
-                    </dt>
-                    <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
-                      {totalConnectionLossBySeconds / 60} phút
-                    </dd>
-                  </div>
-                  <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt className="text-sm font-medium text-gray-700">
-                      Số hoạt động không có video đi kèm
-                    </dt>
-                    <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
-                      {processWithoutObjectDetectionCount}
-                    </dd>
-                  </div>
-                  <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt className="text-sm font-medium text-gray-700">
-                      Số lượng khai báo bị xoá
-                    </dt>
-                    <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
-                      {deletedItemCount}
-                    </dd>
-                  </div>
-                  <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt className="text-sm font-medium text-gray-700">
-                      Số lượng khai báo bị sửa đổi
-                    </dt>
-                    <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
-                      {editItemCount} / {totalItemCount}
-                    </dd>
-                  </div>
-                  <div className="lg:py-3 py-1 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt className="text-sm font-medium text-gray-700">
-                      Số lượng camera
-                    </dt>
-                    <dd className="mt-1 text-sm text-black lg:text-base font-medium sm:mt-0 sm:col-span-2">
-                      {totalCamera}
-                    </dd>
-                  </div>
-                </dl>
-              </div>
-            </div>
-          </div>
-        </DialogBody>
-        0
-        <DialogFooter>
-          <Button variant="gradient" color="red" onClick={handleOpen}>
-            <span>Thoát</span>
-          </Button>
-        </DialogFooter>
-      </Dialog>
     </section>
   );
 };
