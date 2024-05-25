b50b235d08b702b571659a0d87a2f608771c0f50
doandung <doantrungdung2001@gmail.com>
2024-05-25 08:37:02 +0700

    add tables events

--- src/Components/EventFarm/EventFarm.jsx
+++ src/Components/EventFarm/EventFarm.jsx
@@ -3,61 +3,91 @@ import { useParams } from "react-router-dom";
 import useEventFarm from "./useEventFarm";
 
 import "aos/dist/aos.css";
-import { formatDateTime, formatTransactionHashTable } from "../../Utils/helpers";
+import {
+  formatDateTime,
+  formatTransactionHashTable,
+} from "../../Utils/helpers";
 
 const EventFarmComponent = () => {
   const { farmId } = useParams();
-  const { allEventByFarm, isSuccessAllEventByFarm, isLoadingAllEventByFarm } = useEventFarm({
-    farmId,
-  });
+  const { allEventByFarm, isSuccessAllEventByFarm, isLoadingAllEventByFarm } =
+    useEventFarm({
+      farmId,
+    });
 
   return (
     <>
-    {
-        isSuccessAllEventByFarm && allEventByFarm && allEventByFarm.length > 0 &&
-        <div className="pt-[10vh]"> {
-            allEventByFarm.map((event) => {
-                return (
-                    <div key={event.id} className="flex flex-col items-center justify-center">
-                        <div className="flex flex-col items-center justify-center w-11/12 h-40 bg-white rounded-lg shadow-md my-5">
-                            <div className="flex flex-row items-center justify-between w-full h-1/2 px-5 py-2">
-                                <div className="flex flex-row items-center justify-start w-1/2">
-                                    <p className="text-lg font-semibold text-blue-800">{ formatTransactionHashTable({
-                                        str: event.tx,
-                                        a: 8,
-                                        b: 5
-                                    })}</p>
-                                </div>
-                            </div>
-                            <div className="flex flex-row items-center justify-between w-full h-1/2 px-5 py-2">
-                                <div className="flex flex-row items-center justify-start w-1/2">
-                                    <p className="text-lg font-semibold text-gray-800">{formatDateTime(event.timestamp)}</p>
-                                </div>
-                            </div>
-                            <div className="flex flex-row items-center justify-between w-full h-1/2 px-5 py-2">
-                                <div className="flex flex-row items-center justify-start w-1/2">
-                                    <p className="text-lg font-semibold text-gray-800">{event.event}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })
-        }
-        </div>
-    }
-    {
-            isLoadingAllEventByFarm && 
-            <div className="flex flex-col items-center justify-center pt-[10vh]">
-                <p className="text-lg font-semibold text-gray-800">Loading...</p>
+      <section className="py-1 bg-blueGray-50">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-2xl">
+            <div className="rounded-t mb-0 px-4 py-3 border-0">
+              <div className="flex flex-wrap items-center">
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                  <h3 className="font-semibold text-2xl text-blueGray-700 lg:text-3xl">
+                    Bảng thông tin
+                  </h3>
+                </div>
+              </div>
             </div>
-        }
-    {
-        isSuccessAllEventByFarm && (!allEventByFarm || allEventByFarm.length === 0) &&
-        <div className="flex flex-col items-center justify-center pt-[10vh]">
-            <p className="text-lg font-semibold text-gray-800">Không có dữ liệu</p>
+
+            <div className="block w-full overflow-x-auto">
+              <table className="items-center bg-transparent w-full border-collapse">
+                <thead>
+                  <tr>
+                    <th className="px-4 bg-gray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-sm lg:text-xl uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Mã Transaction
+                    </th>
+                    <th className="px-4 bg-gray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-sm lg:text-xl uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Thời gian
+                    </th>
+                    <th className="px-4 bg-gray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-sm lg:text-xl uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Khác
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {isSuccessAllEventByFarm &&
+                    allEventByFarm &&
+                    allEventByFarm.length > 0 &&
+                    allEventByFarm.map((event) => {
+                      return (
+                        <tr key={event.id}>
+                          <th className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs lg:text-xl whitespace-nowrap p-4 text-left text-blue-500 hover:text-blue-00">
+                            {formatTransactionHashTable({
+                              str: event.tx,
+                              a: 8,
+                              b: 5,
+                            })}
+                          </th>
+                          <td className="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs lg:text-xl whitespace-nowrap p-4 text-gray-600">
+                            {formatDateTime(event.timestamp)}
+                          </td>
+                          <td className="border-t-0 px-4 align-center border-l-0 border-r-0 text-xs lg:text-xl whitespace-nowrap p-4 text-gray-600">
+                            {event.event}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-    }
+      </section>
+
+      {isLoadingAllEventByFarm && (
+        <div className="flex flex-col items-center justify-center pt-[10vh]">
+          <p className="text-lg font-semibold text-gray-800">Loading...</p>
+        </div>
+      )}
+      {isSuccessAllEventByFarm &&
+        (!allEventByFarm || allEventByFarm.length === 0) && (
+          <div className="flex flex-col items-center justify-center pt-[10vh]">
+            <p className="text-lg font-semibold text-gray-800">
+              Không có dữ liệu
+            </p>
+          </div>
+        )}
     </>
   );
 };
